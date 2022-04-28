9c404317a1dd3fafaa065954bbb485540c808e5f
MohammedHendy94 <MohammedHendy94@gmail.com>
2022-04-28 13:46:20 +0200

    Ok Third

--- src/test/java/Runners/Runner.java
+++ src/test/java/Runners/Runner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/Features", glue = "Tests" )
+@CucumberOptions(features = "src/test/java/Features", glue = "Tests" , plugin = {"pretty" , "html:target/report/reports.html"} )
 
 public class Runner {
 }
--- src/test/java/Tests/Hooks.java
+++ src/test/java/Tests/Hooks.java
@@ -1,2 +1,17 @@
-package Tests;public class Hooks {
+package Tests;
+
+import VOIS.Base.Base;
+import org.junit.After;
+import org.junit.Before;
+
+public class Hooks extends Base {
+    @Before
+    public void run_before_tests(){
+
+    }
+    // To Be Executed after each test
+    @After
+    public void run_after_tests(){
+        driver.quit();
+    }
 }
